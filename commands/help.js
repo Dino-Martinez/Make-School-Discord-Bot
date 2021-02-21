55a4344b0e4283cef1d22f70356172a3992b7073
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-02-21 13:04:08 -0800

    [Added] Created simple help command

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,19 @@
+const fs = require('fs')
+
 module.exports = {
   name: 'help',
   description: 'Sends a list of commands with information.',
   execute (message, args) {
-    message.channel.send('This command is a work in progress!')
+    const { channel } = message
+    let commandList = 'Here is a list of my commands:'
+    const commandFiles = fs
+      .readdirSync('./commands')
+      .filter(file => file.endsWith('.js'))
+    for (const file of commandFiles) {
+      const command = require(`../commands/${file}`)
+      commandList += `\n${command.name} - ${command.description}`
+    }
+
+    channel.send(commandList)
   }
 }
