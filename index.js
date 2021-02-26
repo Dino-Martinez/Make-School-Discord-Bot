1d7411e412d3d48f86260e6a0742e3c4b8ebdb11
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-02-25 16:32:05 -0800

    [Hotfix] Edited the bot's status

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ const cooldowns = new Discord.Collection()
 // Log message so we know the bot is ready for use
 client.once('ready', () => {
   console.log('Ready!')
+  // Set bot presence
+  client.user.setPresence({
+    activity: {
+      name: 'your problems :)',
+      type: 'LISTENING',
+      details: 'Type !help for a list of my commands and features!'
+    },
+    status: 'idle'
+  })
 })
 
 client.login(process.env.BOT_TOKEN)
