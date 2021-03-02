756dc51a2825d7e714d683174125f42c94769485
Chai <chaiyawahoo@gmail.com>
2021-03-01 18:31:13 -0800

    [Hotfix] Remove MessageEmbed author and footer

--- src/EmbedWrapper.js
+++ src/EmbedWrapper.js
@@ -4,8 +4,6 @@ class EmbedWrapper extends Discord.MessageEmbed {
   constructor (title) {
     super()
       .setColor('#ffbad2')
-      .setAuthor('Rich Embeds', 'https://i.imgur.com/wSTFkRM.png')
-      .setFooter('Courtesy of custom bot')
       .setTimestamp()
       .setTitle(title)
   }
