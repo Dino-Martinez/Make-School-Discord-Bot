27490cf7b739637ee048ce771547a39b9761290a
Chai <chaiyawahoo@gmail.com>
2021-03-16 22:38:41 -0700

    [Added] config command

---
+++ src/commands/utilities/config.js
@@ -0,0 +1,58 @@
+const { readFileSync, writeFileSync } = require('fs')
+const path = require('path')
+const { EmbedWrapper } = require('../../EmbedWrapper.js') 
+
+module.exports = {
+    name: 'config',
+    aliases: ['setup', 'cfg'],
+    description:
+      'Edit bot config.',
+    usage: '<key> <value>',
+    minArgs: 0,
+    cooldown: 2,
+    dmCommand: true,
+    guildCommand: true,
+    async execute (props) {
+      // Destructure the things we need out of props
+      const { message, args } = props
+      const { guild, channel } = message
+      let filePath = path.resolve(__dirname, '../../../config/bot-config.json')
+      let rawData = readFileSync(filePath)
+      let botConfig = JSON.parse(rawData)
+      const response = new EmbedWrapper('Here is my current configuration:')
+  
+      if (!guild || guild.available) {
+        // If no arguments, send existing information
+        if (args.length === 0) {
+          Object.keys(botConfig).forEach(key => {
+            response.addField(key, botConfig[key])
+          })
+        } else if (args.length === 1) {
+          // User is passing 1 argument
+          if (botConfig.hasOwnProperty(args[0])) {
+            response.addField(args[0], botConfig[args[0]])
+          } else {
+            return channel.send("That key does not exist!")
+          }
+        } else if (args.length === 2) {
+          // User is passing 2 arguments
+          if (botConfig.hasOwnProperty(args[0])) {
+            botConfig[args[0]] = args[1]
+            Object.keys(botConfig).forEach(key => {
+              response.addField(key, botConfig[key])
+            })
+            writeFileSync(filePath, JSON.stringify(botConfig, undefined, 2))
+          } else {
+            // > 2 arguments
+            return channel.send("That key does not exist!")
+          }
+        } else {
+          return channel.send("You are sending too many arguments.")
+        }
+        return channel.send(response)
+      } else {
+        throw new Error('The guild is not available')
+      }
+    }
+  }
+  
