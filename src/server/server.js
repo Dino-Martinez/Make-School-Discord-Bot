6e81151410a2948c789e7b1f5be45a11d6f90b88
Tristan Thompson <tristan15thompson@gmail.com>
2021-03-16 20:39:55 -0700

    [EDIT] continuing express server startup

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,16 @@
 //Require Libraries
-const express = require('express')
-import bodyParser from 'body-parser'
+const express = require('express');
+const bodyParser = require ('body-parser')
+const {google} = require('googleapis');
+const readline = require('readline');
+
+
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+const credentials = require('./credentials.json');
+const {client_secret, client_id, redirect_uris} = credentials.installed;
+const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
 
 //Use Express
 const app = express()
@@ -12,6 +22,21 @@ app.get('/', (req, res) => {
   let dID = ''
   if( req.student.dID ) {
     dID = req.student.dID
+  }
+
+  const student = students.get(author.id)
+
+  if(!student) {
+      return
+    }
+  else {
+    if (!student.token) {
+        //Get token from OAuth2 and save token to a student
+        const token = getAccessToken(oAuth2Client, callback, student);
+        oAuth2Client.setCredentials(JSON.parse(token));
+    } else {
+      // listEvents
+    }
   }
 });
 
