5af7d44422b245f804fe9b0aaa0584ca970a09b0
Chai <chaiyawahoo@gmail.com>
2021-03-18 14:51:20 -0700

    [Added] More config support

--- src/commands/utilities/config.js
+++ src/commands/utilities/config.js
@@ -21,33 +21,55 @@ module.exports = {
     const botConfig = JSON.parse(rawData)
     const response = new EmbedWrapper('Here is my current configuration:')
 
+    const addConfigToResponse = () => {
+      Object.keys(botConfig).forEach(key => {
+        if (Array.isArray(botConfig[key]) && botConfig[key].length === 0) {
+          response.addField(key, '[]')
+        } else {
+          response.addField(key, botConfig[key])
+        }
+      })
+    }
+
     if (!guild || guild.available) {
       // If no arguments, send existing information
       if (args.length === 0) {
-        Object.keys(botConfig).forEach(key => {
-          response.addField(key, botConfig[key])
-        })
+        addConfigToResponse()
       } else if (args.length === 1) {
         // User is passing 1 argument
         if (Object.prototype.hasOwnProperty.call(botConfig, args[0])) {
           response.addField(args[0], botConfig[args[0]])
         } else {
-          return channel.send('That key does not exist!')
+          return channel.send(`The key \`${args[0]}\` does not exist!`)
         }
       } else if (args.length === 2) {
-        // User is passing 2 arguments
-        if (Object.prototype.hasOwnProperty.call(botConfig, args[0])) {
-          botConfig[args[0]] = args[1]
+        if (args[0] == "clear" && Array.isArray(botConfig[args[1]])) {
+          botConfig[args[1]] = []
+          addConfigToResponse()
+          writeFileSync(filePath, `${JSON.stringify(botConfig, undefined, 2)}\n`)
+        } else if (Object.prototype.hasOwnProperty.call(botConfig, args[0])) {
+          if (typeof botConfig[args[0]] == "string") {
+            botConfig[args[0]] = args[1]
+          } else {
+            botConfig[args[0]] = [ args[1] ]
+          }
           Object.keys(botConfig).forEach(key => {
             response.addField(key, botConfig[key])
           })
           writeFileSync(filePath, `${JSON.stringify(botConfig, undefined, 2)}\n`)
         } else {
-          // > 2 arguments
-          return channel.send('That key does not exist!')
+          return channel.send(`The key \`${args[0]}\` does not exist!`)
         }
       } else {
-        return channel.send('You are sending too many arguments.')
+        if (Object.prototype.hasOwnProperty.call(botConfig, args[0])) {
+          if (Array.isArray(botConfig[args[0]])) {
+            botConfig[args[0]] = args.slice(1)
+            addConfigToResponse()
+            writeFileSync(filePath, `${JSON.stringify(botConfig, undefined, 2)}\n`)
+          } else {
+            return channel.send('You are sending too many arguments.')
+          }
+        }
       }
       return channel.send(response)
     } else {
