774076952561d29d2484393ca239f512474fdc83
Chai <chaiyawahoo@gmail.com>
2021-03-18 20:14:28 -0700

    [Added] Ban and Kick functionality

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,36 +1,26 @@
-const { EmbedWrapper } = require('../../EmbedWrapper.js')
-
 module.exports = {
   name: 'ban',
   aliases: [],
   description: 'Bans a specified user.',
   usage: '<@User>',
-  minArgs: 0,
+  minArgs: 1,
   cooldown: 2,
   dmCommand: false,
   guildCommand: true,
-  execute (props) {
+  async execute (props) {
     // Destructure the things we need out of props
     const { message } = props
     const { guild, channel } = message
 
     // Check that the guild is available for processing
     if (guild.available) {
-
-      // Add our fields
-      response.addFields(
-        {
-          name: 'Owned by:',
-          value: ownerName
-        },
-        {
-          name: 'Member Count:',
-          value: memberCount
-        }
-      )
-
-      // Send formatted string back to user
-      channel.send(response)
+      const member = message.mentions.members.first()
+      if (member.bannable) {
+        member.ban()
+        return channel.send(`User ${member.displayName} was banned.`)
+      } else {
+        return channel.send(`Cannot ban ${member.displayName}.`)
+      }
     } else {
       throw new Error('The guild is not available')
     }
